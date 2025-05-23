d05e47e545ac4952e2c28aa1ffa675759d270d79
NikoHelle <niko.helle@hyperactive.fi>
2025-05-23 23:11:27 +0300

    Fix 404 again

--- app/components/DynamicContent.tsx
+++ app/components/DynamicContent.tsx
@@ -5,6 +5,7 @@ import dynamic from "next/dynamic";
 const GreenLeasingSection = dynamic(() => import("../components/GreenLeasingSection"), { ssr: false });
 const PersonnelSection = dynamic(() => import("../components/PersonnelSection"), { ssr: false });
 const NewsOrBlogSection = dynamic(() => import("../components/NewsOrBlogSection"), { ssr: false });
+const InnoFleetSection = dynamic(() => import("../components/InnoFleetSection"), { ssr: false });
 
 export default function DynamicContent({ locale }: { locale: string }) {
   return (
@@ -12,6 +13,7 @@ export default function DynamicContent({ locale }: { locale: string }) {
       <NewsOrBlogSection locale={locale} />
       <PersonnelSection />
       <GreenLeasingSection />
+      <InnoFleetSection />
     </>
   );
 }
