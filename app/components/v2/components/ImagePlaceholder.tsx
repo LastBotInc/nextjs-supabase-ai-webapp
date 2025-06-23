1b9bfa4693f3a4068153b6ab53296418393a709e
NikoHelle <niko.helle@hyperactive.fi>
2025-06-23 12:33:22 +0300

    Customer stories page

--- app/components/v2/components/ImagePlaceholder.tsx
+++ app/components/v2/components/ImagePlaceholder.tsx
@@ -5,7 +5,7 @@ export function ImagePlaceholder({
   aspectRatio,
   className,
   ...rest
-}: HTMLAttributes<HTMLDivElement> & { aspectRatio: "16:9" | "4:3" | "1:1" }) {
+}: HTMLAttributes<HTMLDivElement> & { aspectRatio: "16:9" | "4:3" | "1:1" | "3:4" }) {
   return (
     <div
       className={cn("w-full text-white bg-gray-600 flex items-center justify-center rounded-lg", className)}
